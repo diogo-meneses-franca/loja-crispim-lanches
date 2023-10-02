c2b1593bb56d5c865dfb2eb8e9d416d59e707c14
diogo-meneses-franca <diogo.meneses.franca@gmail.com>
2023-10-01 23:25:11 -0300

    Iniciado o envio de imagens para a nuvem

--- front-end/loja-crispim/src/components/product/index.js
+++ front-end/loja-crispim/src/components/product/index.js
@@ -6,6 +6,9 @@ import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from "primereact/inputtext";
 import { InputSwitch } from "primereact/inputswitch";
+import { FileUpload } from 'primereact/fileupload';
+import { ImageCompressor } from 'image-compressor';
+import AWS from 'aws-sdk';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import 'primeicons/primeicons.css';
@@ -16,7 +19,7 @@ const Product = () => {
     const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 20 });
     const [productPage, setProductPage] = useState([]);
     const [editMode, setEditMode] = useState(false)
-    const [product, setProduct] = useState({ name: '' })
+    const [product, setProduct] = useState({ name: '', description: '', costValue: '', saleValue: '', images: [], category: {}, brand: {} })
     const productService = new ProductService();
 
     useEffect(() => {
@@ -34,7 +37,8 @@ const Product = () => {
     };
 
     const handleChange = (event) => {
-        setProduct({ ...product, name: event.target.value });
+        const {name, value} = event.target;
+        setProduct({ ...product, [name]: value });
     }
 
     const requestProduct = (pagination) => {
@@ -141,16 +145,69 @@ const Product = () => {
     const imageBodyTemplate = (product) => {
         return <img src={product.images[0].url} className="product-image" onClick={()=> setOpen(true)}/>;
     };
+
+    AWS.config.update({
+        accessKeyId: 'chave',
+        secretAccessKey: 'senha'
+    });
+
+    const s3 = new AWS.S3();
+
+    const handleFileUpload = async (event) => {
+        console.log(event.file.name)
+        const files = event.files;
+        console.log(files)
+        if(!files || files.length === 0) return;
+
+        for(const file of files){
+            try{
+                const compressedImage = await new ImageCompressor(file, {quality: 0.6}).compress();
+                const fileName = `uploads/${Date.now()}-${compressedImage.name}`;
+
+                const params = {
+                    Bucket: 'loja-crispim' ,
+                    key: fileName,
+                    Body: compressedImage
+                }
+                await s3.upload(params).promise();
+                const imageUrl = `http://loja-crispim.s3.amazonaws.com/${fileName}`
+
+                console.log("Uploaded image:", imageUrl);
+            }catch(error){
+                console.log('Error uploading image: ', error);
+            }
+        }
+
+    }
+    const handleUploadResponse = (response) => {
+        if (response.success) {
+          console.log('Image uploaded successfully:', response.fileName);
+          // Handle success, e.g., update your UI or store the S3 URL
+        } else {
+          console.error('Image upload failed:', response.error);
+          // Handle the error, e.g., display an error message to the user
+        }
+      };
     return (
         <div className="product-view">
             <div>
-                <Button label="Cadastrar" onClick={() => setOpen(true)} />
+                <Button className="form-button" label="Cadastrar" onClick={() => setOpen(true)} />
             </div>
             <div className="card flex justify-content-center">
                 <Dialog header={editMode ? "Editar Produto" : "Cadastrar Produto"} visible={open} onHide={handleDialogClose}
                     style={{ width: '50vw' }}>
-                    <InputText value={product.name} onChange={handleChange} placeholder="Ex: Coca-Cola" />
-                    <Button className="dialog-button" severity="success" label="Confirmar" size="small" onClick={onSave} />
+                    <form >
+                        <label htmlFor="name">Nome</label>
+                        <InputText name="name" value={product.name} onChange={handleChange} placeholder="Ex: Coca-Cola" required={true} />
+                        <label htmlFor="description">Descrição</label>
+                        <InputText name="description" value={product.description} onChange={handleChange} placeholder="Ex: Coca-Cola" required={false} />
+                        <label htmlFor="costPrice">Preço de custo</label>
+                        <InputText name="costValue" value={product.costValue} onChange={handleChange} required={true} />
+                        <label htmlFor="salePrice">Preço de venda</label>
+                        <InputText value={product.saleValue} name="saleValue" onChange={handleChange} required={true}/>
+                        <FileUpload className="file-sender" customUpload={handleFileUpload} onUpload={handleUploadResponse} name="images" multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Arraste e solte as imagens aqui.</p>} />
+                        <Button severity="success" label="Confirmar" size="small" onSubmit={onSave} />
+                    </form>
                 </Dialog>
             </div>
             <div className="card">
@@ -167,6 +224,7 @@ const Product = () => {
                     }}>
                     <Column field="name" header="Nome" sortable style={{ width: '15%' }}></Column>
                     <Column field="images[0]" header="Imagem" body={imageBodyTemplate}></Column>
+                    <Column field="description" header="Descrição"></Column>
                     <Column field="costValue" body={costValueBodyTemplate} header="custo" sortable style={{ width: '7%' }}></Column>
                     <Column field="saleValue" body={saleValueBodyTemplate} header="venda" sortable style={{ width: '7%' }}></Column>
                     <Column field="category.name" header="Categoria" sortable style={{ width: '8%' }}></Column>
